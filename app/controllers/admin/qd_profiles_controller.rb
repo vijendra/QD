a741bd7f73bba55b0ca031fc20b7a37b948c5ec7
isiri <isiri@isiri-desktop.(none)>
2009-05-12 18:40:56 +0530

    qd profile search logic added

--- app/controllers/admin/qd_profiles_controller.rb
+++ app/controllers/admin/qd_profiles_controller.rb
@@ -3,11 +3,20 @@ class Admin::QdProfilesController < ApplicationController
   layout 'admin'
 
   def index
-    @qd_profiles = QdProfile.all
+  	@search = QdProfile.new_search(params[:search])
+    @search.per_page ||= 2
+    @qd_profiles = @search.all
 
-    respond_to do |format|
+   respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @qd_profiles }
+      format.js {  render :update do |page|
+      	            page.replace_html 'qd_profile-list', :partial => 'qd_profiles_list'
+     	           end
+      	        }
     end
-  end
+ end
+
+
+
 end
