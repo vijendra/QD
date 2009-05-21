7702af266f7eb2a0404822aee3a4a2e2f64ae214
isiri <isiri@isiri-desktop.(none)>
2009-05-21 18:48:56 +0530

    assign administrator to dealer

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -4,4 +4,8 @@ class Administrator < User
 
   accepts_nested_attributes_for :administrator_profile
 
+   def self.administrators_list
+     self.find(:all).collect{|admin| [admin.login, admin.id] }
+  end
+
 end
