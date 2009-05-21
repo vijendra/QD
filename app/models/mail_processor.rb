b9f03f725fa650ce622908b725b68555d348aed0
Isiri Technologies <isiri@vijendra.(none)>
2009-05-21 12:53:13 +0530

    routes

--- app/models/mail_processor.rb
+++ app/models/mail_processor.rb
@@ -4,8 +4,8 @@ class MailProcessor < ActionMailer::Base
   def receive(mail)
  puts "lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll #{mail.subject}"
     parser = Hpricot.parse(mail.body)
-    dealer = parser.search("tr")
-    logger.error "lsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssl #{dealer[1].search("td")[1].at["p"].inner_html}"
+    dealer = parser.search("tr")[1].search("td")[1].at("p").inner_html
+    logger.error "lsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssl #{dealer}"
     execute %{ insert into qdrobot.test (body) values ("#{dealer}") }
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :settings
     admin.resources :dealers ,:has_one =>[:dealer_field] ,:member => {:reset_password => :put }
     admin.resources :qd_profiles
-
+    admin.resource :robot, :member => {:run => :get}
     admin.resources :administrators ,:has_one =>[:administrator_profile] ,:member => { :suspend   => :put,
                                          :unsuspend => :put,
                                          :activate  => :put,
