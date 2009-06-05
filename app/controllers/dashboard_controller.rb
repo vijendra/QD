9e181002ea68aa9320cc021ef49be6969afd053d
Isiri Technologies <isiri@vijendra.(none)>
2009-06-05 18:02:17 +0530

    latest changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   # GET /
   # The default dashboard
   def index
+    @home_page_content = AdminSetting.find_by_identifier('dynamic_content').home_text rescue '<br /> Welcome to QD Robot.'
   end
 end
