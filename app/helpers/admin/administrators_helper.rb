f42febef8070c3959c814e1cbbce31f6f06b5eb0
isiri <isiri@isiri-desktop.(none)>
2009-06-05 13:38:56 +0530

    active and inactive dealer for admin

--- app/helpers/admin/administrators_helper.rb
+++ app/helpers/admin/administrators_helper.rb
@@ -1,7 +1,8 @@
 module Admin::AdministratorsHelper
 
 	def acitive_inactive_dealer(id,text)
-    count =0
+		count = 0
+		count = Profile.find(:all,:include =>:user , :conditions => {:administrator_id => "#{id}" , :user => {:state => "#{text}"} } ).size
 
 		count.nil? ?  0 : count
 	end
