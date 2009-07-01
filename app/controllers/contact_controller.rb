ae213be501b50c6a24c12ec45d9ac9ab4027f896
isiri <isiri@isiri-desktop.(none)>
2009-07-01 13:49:17 +0530

    contact changes

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,22 @@
+class ContactController < ApplicationController
+	before_filter :check_logged_in
+
+  def super_admin_contact
+  	@user = User.find(126)
+  	#render :lauout =>"false"
+  end
+
+  def administrator_contact
+  	@administrator = Administrator.find(current_user.administrator_id) unless current_user.administrator_id.blank?
+  	#render :lauout =>"false"
+  end
+
+  private
+
+   def check_logged_in
+   	  unless logged_in?
+   	 	  redirect_to (:controller => :session ,:action => :new )
+  	  end
+   end
+
+end
