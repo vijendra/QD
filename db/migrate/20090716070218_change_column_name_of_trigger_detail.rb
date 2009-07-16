8236cd57be441de0da080d1969f26d3b1813dab4
isiri <isiri@isiri-desktop.(none)>
2009-07-16 12:49:12 +0530

    database changes

---
+++ db/migrate/20090716070218_change_column_name_of_trigger_detail.rb
@@ -0,0 +1,15 @@
+class ChangeColumnNameOfTriggerDetail < ActiveRecord::Migration
+  def self.up
+    change_column :trigger_details, :marked, :string, :default => 'no'
+    
+    records = TriggerDetail.find(:all ,:conditions => ["marked IS null"])
+    records.each do | rec|
+      rec.marked = 'no'
+      rec.save!
+    end
+    
+  end
+
+  def self.down
+  end
+end
