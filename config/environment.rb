b3917a6150ff2e9dd8e06f80b7bd3a72c3ba3be4
isiri <isiri@Vijendra-desktop.(none)>
2009-09-23 13:17:42 +0530

    removed deleted images

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,10 @@
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -49,16 +49,17 @@ Rails::Initializer.run do |config|
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "mail.isiritech.net",
-    :port => 25,
-    :domain => "isiritech.net",
-    :authentication => :login,
-    :user_name => "qdrobot@isiritech.net",
-    :password => "qdrobot"
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "qdrllc.com",
+    :authentication => :plain,
+    :user_name => "support@qdrllc.com",
+    :password => "password01"
 }
 
 end
 
-ExceptionNotifier.exception_recipients = %w(vijendrakarkala@gmail.com)
-ExceptionNotifier.sender_address = %("QD_ROBOT" <qdrobot@isiritech.net>)
+ExceptionNotifier.exception_recipients = %w(vijendrakarkala@gmail.com support@qdrllc.com)
+ExceptionNotifier.sender_address = %(support@qdrllc.com)
 ExceptionNotifier.email_prefix = "[QDROBOT_ERROR] "
