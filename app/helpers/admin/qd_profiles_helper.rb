bc84688e100999739d4d0441f66a9eca63446854
isiri <isiri@isiri-desktop.(none)>
2011-06-15 14:01:38 +0530

    administrator

--- app/helpers/admin/qd_profiles_helper.rb
+++ app/helpers/admin/qd_profiles_helper.rb
@@ -8,4 +8,11 @@ module Admin::QdProfilesHelper
     end
   end
 
+  def check_for_pending_append
+    dealer_ids = current_user.dealers.collect(&:id)
+    return false if dealer_ids.blank?
+    return DataAppend.find(:first, :conditions => ['status_message = ? and dealer_id in (?)', 'sent', dealer_ids ])? true : false
+  end
+
 end
+
