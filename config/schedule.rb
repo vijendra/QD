2a7bc9f50e9420420875c54a7db6e45f9af63bca
isiri <isiri@isiri-desktop.(none)>
2011-06-22 16:11:17 +0530

    schedule

---
+++ config/schedule.rb
@@ -0,0 +1,14 @@
+set :output, {:error => "/home/rails/d2leads/log/whenever_error.txt", :standard => "/home/rails/d2leads/log/whenever_success.txt" }
+env :PATH, "/usr/local/bin:$PATH"
+
+
+every 2.minutes do
+  runner "DncNumber.send_for_dnc('weekly')"
+end
+every 15.days do
+  runner "DncNumber.send_for_dnc('15 days')"
+end
+every 30.days do
+  runner "DncNumber.send_for_dnc('monthly')"
+end
+
